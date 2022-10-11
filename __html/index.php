3a455011e407da9a85d430ee6562718760d675ca
albax <bacz.al@gmail.com>
2022-10-11 01:23:50 -0500

    html

---
+++ __html/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> HTML </title>
+</head>
+<body>
+    <div>
+        <span> es parrafo </span>
+        <span> <h2> titulo h2 </h2></span>
+    </div>
+</body>
+</html>
